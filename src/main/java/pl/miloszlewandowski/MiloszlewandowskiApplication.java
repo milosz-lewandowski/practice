fc0ba27c656d47fd8b98712bf45a759a503da9c5
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-03-31 05:01:40 +0200

    MiloszlewandowskiApplication: Home page & JustHappened page

---
+++ src/main/java/pl/miloszlewandowski/MiloszlewandowskiApplication.java
@@ -0,0 +1,16 @@
+package pl.miloszlewandowski;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MiloszlewandowskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiloszlewandowskiApplication.class, args);
+	}
+
+	//Tutorial accesingDataJPA
+}
