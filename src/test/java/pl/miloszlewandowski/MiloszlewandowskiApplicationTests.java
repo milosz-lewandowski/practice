18043d12e4c39179ca49906a4ff87ec02a293ddd
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-04-04 20:59:45 +0200

    Customer & Orders REST api with swagger-ui configuration files

---
+++ src/test/java/pl/miloszlewandowski/MiloszlewandowskiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloszlewandowskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
