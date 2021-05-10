3b40ea60ac856af0c0ca2a2155428c4b062945b9
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-10 19:11:36 +0200

    X and O players swap after their move

--- reacttut/src/index.js
+++ reacttut/src/index.js
@@ -16,12 +16,16 @@ class Board extends React.Component {
     constructor(props) {
         super(props)
         this.state = {squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'x' : 'o';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
     renderSquare(i) {
         return (
@@ -33,7 +37,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + this.state.xIsNext;
 
         return (
             <div>
