7042677956b6f05811d948cba153af1c789156e4
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-10 18:53:00 +0200

    function component

--- reacttut/src/index.js
+++ reacttut/src/index.js
@@ -4,31 +4,32 @@ import './index.css';
 import App from './App';
 
 
-class Square extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={ () => {
-                    this.setState({value: 'x'})
-                }}
-            >
-                {this.state.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {squares: Array(9).fill(null),
+        };
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares})
+    }
     renderSquare(i) {
-        return <Square value ={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -58,6 +59,16 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+    renderSquare(i) {
+        return (<Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+
     render() {
         return (
             <div className="game">
