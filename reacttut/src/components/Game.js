d1a582cdd08e2fc80de7848edebb6ff1f77cadf1
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-11 21:11:24 +0200

    Restart button frontend

--- reacttut/src/components/Game.js
+++ reacttut/src/components/Game.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Board} from "./Board";
+import { Button } from 'react-bootstrap';
+
 
 export class Game extends React.Component {
     render() {
@@ -8,6 +10,10 @@ export class Game extends React.Component {
                 <div className="game-board">
                     <Board />
                 </div>
+                    <Button
+                        className={"restart-game-button"}
+                        // TODO onClick={props.onClick}
+                    >Restart game</Button>
                 <div className="game-info">
                     <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
