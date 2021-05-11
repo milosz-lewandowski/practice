3570d22737e17608401ec18005d4de87048cf393
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-11 19:08:19 +0200

    Square.js

---
+++ reacttut/src/components/Square.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
