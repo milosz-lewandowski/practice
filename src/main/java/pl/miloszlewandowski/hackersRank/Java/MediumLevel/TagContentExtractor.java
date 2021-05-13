bbc17d0fcfaa1ed753a00ea0424c6610da353cae
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-13 19:17:32 +0200

    Hackerrank TagContentExtractor exercise

--- src/main/java/pl/miloszlewandowski/hackersRank/Java/MediumLevel/TagContentExtractor.java
+++ src/main/java/pl/miloszlewandowski/hackersRank/Java/MediumLevel/TagContentExtractor.java
@@ -1,19 +1,36 @@
 package pl.miloszlewandowski.hackersRank.Java.MediumLevel;
 
 import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *  I have spent like 2 hours trying to figure this out and I gave up,
+ *  This solution is taken from hackerrank exercise discussion
+ */
 
 public class TagContentExtractor {
+
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int testCases = Integer.parseInt(scan.nextLine());
 
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        while(testCases>0){
-            String line = in.nextLine();
+        while (testCases-- > 0) {
+            String line = scan.nextLine();
 
-            //Write your code here
+            boolean matchFound = false;
+            Pattern pattern = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher matcher = pattern.matcher(line);
 
-            testCases--;
+            while (matcher.find()) {
+                System.out.println(matcher.group(2));
+
+                matchFound = true;
+            }
+            if ( ! matchFound) {
+                System.out.println("None");
+            }
         }
     }
 }
-
