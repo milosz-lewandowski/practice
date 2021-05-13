1ae26af580546b63be989c8ec4ec18e8b755f8a8
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-13 23:48:03 +0200

    HackerRank JavaDequeue exercise init

---
+++ src/main/java/pl/miloszlewandowski/hackersRank/Java/MediumLevel/JavaDequeue.java
@@ -0,0 +1,20 @@
+package pl.miloszlewandowski.hackersRank.Java.MediumLevel;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class JavaDequeue {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque<>();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+
+        }
+    }
+}
+
