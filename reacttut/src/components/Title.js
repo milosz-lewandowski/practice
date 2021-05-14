6b350ab893b0a86a1b9984394cde0d59944b58ac
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 02:55:00 +0200

    Title and Badge component

---
+++ reacttut/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Badge} from "react-bootstrap";
+
+export class Title extends React.Component{
+    render() {
+        return(
+            <h1>
+                Miłosz Lewandowski <Badge variant="primary">JakSzybciutko</Badge>
+            </h1>
+        )
+    }
+}
