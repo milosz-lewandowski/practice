5e4206253870a3b93b1bee5df82a0b19878f03cb
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 05:25:28 +0200

    Navbar menu adjusted

--- reacttut/src/components/NavigationBar.js
+++ reacttut/src/components/NavigationBar.js
@@ -6,7 +6,6 @@ export class NavigationBar extends React.Component{
     render() {
         return(
             <Navbar bg="light" expand="lg" >
-
                 <Nav className="justify-content-center">
                         <NavDropdown title="My projects" id="basic-nav-dropdown">
                             <NavDropdown.Item href="https://miloszlewandowski.pl/justhappened">JustHappened description</NavDropdown.Item>
@@ -15,10 +14,9 @@ export class NavigationBar extends React.Component{
                             <NavDropdown.Item href="https://miloszlewandowski.pl/therapists">JustHappened Therapists API test</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Technical details [TODO]</NavDropdown.Item>
                         </NavDropdown>
-
                     </Nav>
-                    <NavItem href="#link">My resume</NavItem>
-                    <NavItem  href="#home">About me</NavItem>
+                    <Nav.Link href="#link" >My resume</Nav.Link>
+                    <Nav.Link href="#home">About me</Nav.Link>
             </Navbar>
         )
     }
