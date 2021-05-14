79bfc1fff8f310b6f1d923ddfd01544a6ed577e6
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 05:01:08 +0200

    Accordion Toggle Variant = text

--- reacttut/src/components/NavigationBar.js
+++ reacttut/src/components/NavigationBar.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button, NavLink, NavItem} from "react-bootstrap";
 
 
 export class NavigationBar extends React.Component{
     render() {
         return(
             <Navbar bg="light" expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav">Menu</Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
+                {/*<Navbar.Toggle aria-controls="basic-navbar-nav">Menu</Navbar.Toggle>*/}
+                {/*<Navbar.Collapse id="basic-navbar-nav">*/}
                     <Nav className="mr-auto">
                         <NavDropdown title="My projects" id="basic-nav-dropdown">
                             <NavDropdown.Item href="https://miloszlewandowski.pl/justhappened">JustHappened description</NavDropdown.Item>
@@ -16,10 +16,12 @@ export class NavigationBar extends React.Component{
                             <NavDropdown.Item href="https://miloszlewandowski.pl/therapists">JustHappened Therapists API test</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Technical details [TODO]</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#link">My resume</Nav.Link>
-                        <Nav.Link href="#home">About me</Nav.Link>
+                        {/*<Nav.Link href="#link">My resume</Nav.Link>*/}
+                        {/*<Nav.Link href="#home">About me</Nav.Link>*/}
                     </Nav>
-                </Navbar.Collapse>
+                <NavItem href="#link">My resume</NavItem>
+                <NavItem  href="#home">About me</NavItem>
+                {/*</Navbar.Collapse>*/}
             </Navbar>
         )
     }
