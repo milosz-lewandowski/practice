a2438146b119c676c1de9b29f2a64381c1e099b4
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 02:54:33 +0200

    Title and Badge component attach

--- reacttut/src/components/App.js
+++ reacttut/src/components/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import {Container, Row, Col} from "react-bootstrap";
 import {Game} from "./Game";
 import {NavigationBar} from "./NavigationBar";
+import {Title} from "./Title";
 
 export class App extends React.Component{
     render(){
         return(
             <Container>
+                <Row>
+                    <Col>
+                        <Title />
+                    </Col>
+                </Row>
                 <Row>
                     <Col>
                         <NavigationBar />
