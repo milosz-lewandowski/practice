fd2917edf2f43011e6f1d20500ebcf86f5ddcc5a
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 03:49:30 +0200

    Attach MusicSection.js and GameSection.js

---
+++ reacttut/src/components/SectionsContainer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Accordion, Card, Button} from "react-bootstrap";
+import {Game} from "./Game";
+import {MusicSection} from "./MusicSection";
+
+
+export class SectionsContainer extends React.Component{
+    render() {
+        return(
+            <Accordion defaultActiveKey="0">
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                            Music Section
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            <MusicSection />
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Game Section
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                        <Card.Body>
+                            <Game />
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+        )
+    }
+}
