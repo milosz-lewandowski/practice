0dd6e2f1c0be131cc5584a646386e9ef0df3d014
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 02:26:39 +0200

    Attach Navigation Bar

--- reacttut/src/components/App.js
+++ reacttut/src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {Container, Row, Col} from "react-bootstrap";
 import {Game} from "./Game";
+import {NavigationBar} from "./NavigationBar";
 
 export class App extends React.Component{
     render(){
         return(
             <Container>
                 <Row>
-                    <Col></Col>
+                    <Col>
+                        <NavigationBar />
+                    </Col>
                 </Row>
                 <Row>
                     <Col style={{textAlign: 'center', border: '1px solid black'}}>
