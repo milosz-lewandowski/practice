42fd276c4bef1b77a77ba88b89b4f8e7b9645190
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 03:46:31 +0200

    GameSection.js

---
+++ reacttut/src/components/GameSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import {Game} from "./Game";
+
+export class GameSection extends React.Component{
+    render() {
+        return(
+            <Game />
+        )
+    }
+}
