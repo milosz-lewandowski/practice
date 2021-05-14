6f4ea47ac9a2210053d0bf2d7a368c41685c647f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 04:38:12 +0200

    Navigation Bar order update

--- reacttut/src/components/NavigationBar.js
+++ reacttut/src/components/NavigationBar.js
@@ -9,17 +9,16 @@ export class NavigationBar extends React.Component{
                 <Navbar.Toggle aria-controls="basic-navbar-nav">Menu</Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">About me</Nav.Link>
-                        <Nav.Link href="#link">My resume</Nav.Link>
-                        <NavDropdown title="Moje projekty" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="https://miloszlewandowski.pl/">Previous static page</NavDropdown.Item>
+                        <NavDropdown title="My projects" id="basic-nav-dropdown">
                             <NavDropdown.Item href="https://miloszlewandowski.pl/justhappened">JustHappened description</NavDropdown.Item>
                             <NavDropdown.Divider />
+                            <NavDropdown.Item href="https://miloszlewandowski.pl/">Previous static page</NavDropdown.Item>
                             <NavDropdown.Item href="https://miloszlewandowski.pl/therapists">JustHappened Therapists API test</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Technical details [TODO]</NavDropdown.Item>
                         </NavDropdown>
+                        <Nav.Link href="#link">My resume</Nav.Link>
+                        <Nav.Link href="#home">About me</Nav.Link>
                     </Nav>
-
                 </Navbar.Collapse>
             </Navbar>
         )
