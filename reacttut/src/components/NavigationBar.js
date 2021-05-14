62e4d795f7311645399958462e3954fd5e003608
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-14 04:13:06 +0200

    Navigation Bar items update

--- reacttut/src/components/NavigationBar.js
+++ reacttut/src/components/NavigationBar.js
@@ -6,24 +6,20 @@ export class NavigationBar extends React.Component{
     render() {
         return(
             <Navbar bg="light" expand="lg">
-                {/*<Navbar.Brand href="#home">Miłosz Lewandowski</Navbar.Brand>*/}
                 <Navbar.Toggle aria-controls="basic-navbar-nav">Menu</Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Kim jestem</Nav.Link>
-                        <Nav.Link href="#link">Moje CV</Nav.Link>
+                        <Nav.Link href="#home">About me</Nav.Link>
+                        <Nav.Link href="#link">My resume</Nav.Link>
                         <NavDropdown title="Moje projekty" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">JustHappened</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="https://miloszlewandowski.pl/">Previous static page</NavDropdown.Item>
+                            <NavDropdown.Item href="https://miloszlewandowski.pl/justhappened">JustHappened description</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
+                            <NavDropdown.Item href="https://miloszlewandowski.pl/therapists">JustHappened Therapists API test</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Technical details [TODO]</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    {/*<Form inline>*/}
-                    {/*    <FormControl type="text" placeholder="Search" className="mr-sm-2" />*/}
-                    {/*    <Button variant="outline-success">Search</Button>*/}
-                    {/*</Form>*/}
+
                 </Navbar.Collapse>
             </Navbar>
         )
