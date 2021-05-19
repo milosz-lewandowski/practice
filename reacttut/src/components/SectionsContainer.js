1701a62e0e30df38b0199c94da56fa12d57294b7
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-19 14:39:51 +0200

    Technologies container

--- reacttut/src/components/SectionsContainer.js
+++ reacttut/src/components/SectionsContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Accordion, Card, Button} from "react-bootstrap";
 import {Game} from "./Game";
 import {MusicSection} from "./MusicSection";
+import {TechnicalDetails} from "./TechnicalDetails";
 
 
 export class SectionsContainer extends React.Component{
@@ -29,6 +30,18 @@ export class SectionsContainer extends React.Component{
                     <Accordion.Collapse eventKey="0">
                         <Card.Body>
                             <MusicSection />
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="text" eventKey="2">
+                            Technologies
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="2">
+                        <Card.Body>
+                            <TechnicalDetails />
                         </Card.Body>
                     </Accordion.Collapse>
                 </Card>
