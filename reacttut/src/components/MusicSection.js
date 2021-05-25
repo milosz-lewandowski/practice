78ea43254fa0670667465de0e52aa475af5d6dce
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-25 04:51:43 +0200

    music update

--- reacttut/src/components/MusicSection.js
+++ reacttut/src/components/MusicSection.js
@@ -4,14 +4,12 @@ export class MusicSection extends React.Component{
     render() {
         return(
             <div>
+                <iframe width="100%" height="312" scrolling="no" frameBorder="no" allow="autoplay"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1261765333&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"/>
+                <iframe width="100%" height="315" scrolling="no" frameBorder="no" allow="autoplay"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1261766494&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"/>
                 <iframe width="100%" height="120" scrolling="no" frameBorder="no" allow="autoplay"
-                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1015536727&color=%23ff5500&
-                    auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
-                </iframe>
-                <iframe width="100%" height="120" scrolling="no" frameBorder="no" allow="autoplay"
-                    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/327563345&color=%23ff5500&
-                    auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
-                </iframe>
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1015536727&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"/>
             </div>
         )
     }
